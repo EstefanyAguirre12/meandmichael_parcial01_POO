7e763ca78253a8963b374aa406c6fbcf26305611
Andresss21 <Andrs27999@gmail.com>
2020-04-19 00:33:39 -0600

    add: Empresa class

--- Parcial01/src/com/EEAR/x00041319/Empresa.java
+++ Parcial01/src/com/EEAR/x00041319/Empresa.java
@@ -1,6 +1,32 @@
 package com.EEAR.x00041319;
 
+import java.util.List;
+
 public class Empresa {
 
-    //test push numero 2
+    private String nombre;
+    List<Empleado> planilla;
+
+    public void Empresa(String name){
+        this.nombre =nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Empleado> getPlanilla() {
+        return planilla;
+    }
+
+    public void addEmpleado(Empleado datos){
+
+        planilla.add(datos);
+
+    }
+
+    public void quitEmpelado(String Empleado){
+
+        planilla.remove(Empleado);
+    }
 }
