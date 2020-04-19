c9080068b2e71d5b936fe8407fa8b682951c6adb
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2020-04-19 00:37:16 -0600

    Add: AddDocumentos method

--- Parcial01/src/com/EEAR/x00041319/Empleado.java
+++ Parcial01/src/com/EEAR/x00041319/Empleado.java
@@ -16,10 +16,36 @@ public abstract class Empleado {
         documentos=new ArrayList<>();
     }
     public void addDocumentos(Documento documento){
+        try{
+            boolean existe = false;
 
+            for(Documento a : documentos){
+                if(a.getNumero().equals(documento.getNumero()))
+                    existe = true;
+            }
+
+            if(existe)
+                throw new AlreadyExistAppException("Ya existe un documento con ese numero");
+
+            documentos.add(documento);
+        }
+        catch(AlreadyExistAppException ex){
+            System.out.println(ex.getMessage());
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
-    public void removeDocumento(String nombre) {
-
+    public void removeDocumento(String nombre) throws NotFoundException {
+        Documento aux = null;
+        for(Documento doc : documentos){
+            if(doc.getNombre().equals(nombre))
+                aux = doc;
+        }
+        if(aux != null)
+            documentos.remove(aux);
+        else
+            throw new NotFoundException("Aplicacion no encontrada, no es posible desinstalar");
     }
     public ArrayList<Documento> getDocumentos() {
         return documentos;
