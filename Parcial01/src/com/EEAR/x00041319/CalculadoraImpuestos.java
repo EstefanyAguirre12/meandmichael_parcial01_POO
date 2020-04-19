bd6b71b73a5cf1e3797e91902e9b05420aa0d03b
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2020-04-19 00:27:46 -0600

    Add: calculadoraimpuestos class

---
+++ Parcial01/src/com/EEAR/x00041319/CalculadoraImpuestos.java
@@ -0,0 +1,16 @@
+package com.EEAR.x00041319;
+
+public final class  CalculadoraImpuestos {
+    private static double totalRenta;
+    private static double totalISSS;
+    private static double totalAFP;
+
+    public CalculadoraImpuestos(){
+    }
+    public static double calcularPago(Empleado em){
+        return 0;
+    }
+    public static String mostrarTotales(){
+        return "Total Renta: " + totalRenta + ", Total AFP: " + totalAFP+ ", Total ISSS: " + totalISSS;
+    }
+}
