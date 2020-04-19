0c31dcf90cff1511bd5090a49dcaa5a2f566a55c
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2020-04-19 00:29:26 -0600

    Add: exception classes

---
+++ Parcial01/src/com/EEAR/x00041319/AlreadyExistAppException.java
@@ -0,0 +1,7 @@
+package com.EEAR.x00041319;
+
+public class AlreadyExistAppException extends Exception {
+    public AlreadyExistAppException(String message) {
+        super(message);
+    }
+}
---
+++ Parcial01/src/com/EEAR/x00041319/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.EEAR.x00041319;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
