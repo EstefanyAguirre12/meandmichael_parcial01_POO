56e63d748cae8f5bcdfb77bb6d5206a2c60d6f2f
Andresss21 <Andrs27999@gmail.com>
2020-04-19 19:39:05 -0600

    add: validation menu and numbers

--- Parcial01/src/com/EEAR/x00041319/Main.java
+++ Parcial01/src/com/EEAR/x00041319/Main.java
@@ -9,7 +9,7 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws NotFoundException {
-	// write your code here
+        // write your code here
         byte opcion = 0;
         String empresa, nombre, puesto, documento, numerodocumento, otro, nombreempleado;
         double salario = 0;
@@ -26,160 +26,195 @@ public class Main {
         do{
             Menu();
             System.out.println("Opcion: ");
-            opcion = sc.nextByte();
-            sc.nextLine();
-        switch (opcion){
-            case 1:
-                System.out.println("¿Que tipo de empleado desea ingresar?");
-                System.out.println("1. Plaza Fija");
-                System.out.println("2. Servicio Profesional");
-                byte tipo = sc.nextByte();
+
+            if (sc.hasNextByte()){
+                opcion = sc.nextByte();
                 sc.nextLine();
-                switch (tipo){
+
+
+                switch (opcion){
                     case 1:
-                        System.out.print("Nombre: "); nombre = sc.nextLine();
-                        System.out.print("Puesto: "); puesto = sc.nextLine();
-                        System.out.print("Salario: ");
-                        do {
-                            if (sc.hasNextDouble()){
-                                salario = sc.nextDouble();
-                                esNumero = true;
-                            }
-                        } while (esNumero = false);
+                        System.out.println("¿Que tipo de empleado desea ingresar?");
+                        System.out.println("1. Plaza Fija");
+                        System.out.println("2. Servicio Profesional");
+                        byte tipo = sc.nextByte();
+                        sc.nextLine();
+                        switch (tipo){
+                            case 1:
+                                System.out.print("Nombre: "); nombre = sc.nextLine();
+                                System.out.print("Puesto: "); puesto = sc.nextLine();
 
-                        do {
-                            if (sc.hasNextInt()){
-                                System.out.print("Extension: "); extension = sc.nextInt();sc.nextLine();
-                                esNumero = true;
-                            }
-                        } while (esNumero = false);
-
-                        p=new PlazaFija(nombre, puesto, salario, extension);
-                        emp.addEmpleado(p);
-                        do{
-                            System.out.print("Documento: "); documento = sc.nextLine();
-                            System.out.print("Numero de documento: "); numerodocumento = sc.nextLine();
-                            p.addDocumentos(new Documento(documento, numerodocumento));
-                            System.out.println("¿Desea ingresar otro?(S/N)");
-                            otro = sc.nextLine();
-                        }while (otro.equals("S"));
-
-                        break;
-                    case 2:
-                        System.out.print("Nombre: "); nombre = sc.nextLine();
-                        System.out.print("Puesto: "); puesto = sc.nextLine();
-                        do {
-                            if (sc.hasNextDouble()){
-                                System.out.print("Salario: "); salario = sc.nextDouble();
-                                esNumero = true;
-                            }
-                        } while (esNumero = false);
-
-                        System.out.print("Meses de contrato: ");
-                        do {
-                            if (sc.hasNextInt()){
-                                mesescontrato = sc.nextInt();sc.nextLine();
-                                esNumero = true;
-                            }
-                        } while (esNumero = false);
+                                do {
+                                    System.out.print("Salario: ");
+                                    if (sc.hasNextDouble()){
+                                        salario = sc.nextDouble();
+                                        esNumero = true;
+                                    }
+                                    else {
+                                        System.out.println("Ingrese un numero");
+                                        esNumero = false;
+                                        sc.next();
+                                    }
+                                } while (!(esNumero));
 
 
-                        s=new ServicioProfesional(nombre, puesto, salario, mesescontrato);
-                        emp.addEmpleado(s);
-                        do{
-                            System.out.print("Documento: "); documento = sc.nextLine();
-                            System.out.print("Numero de documento: "); numerodocumento = sc.nextLine();
-                            s.addDocumentos(new Documento(documento, numerodocumento));
-                            System.out.println("¿Desea ingresar otro?(S/N)");
-                            otro = sc.nextLine();
-                        }while (otro.equals("S"));
+                                do {
+                                    System.out.print("Extension: ");
+                                    if (sc.hasNextInt()){
+                                        extension = sc.nextInt();sc.nextLine();
+                                        esNumero = true;
 
-                        break;
-                }
-                break;
+                                    }
+                                    else {
+                                        System.out.println("Ingrese un numero entero");
+                                        esNumero = false;
+                                        sc.next();
+                                    }
+                                } while (!(esNumero));
 
-            case 2:
-                String aux2="", aux3="";
+                                p=new PlazaFija(nombre, puesto, salario, extension);
+                                emp.addEmpleado(p);
+                                do{
+                                    System.out.print("Documento: "); documento = sc.nextLine();
+                                    System.out.print("Numero de documento: "); numerodocumento = sc.nextLine();
+                                    p.addDocumentos(new Documento(documento, numerodocumento));
+                                    System.out.println("¿Desea ingresar otro?(S/N)");
+                                    otro = sc.nextLine();
+                                }while (otro.equals("S"));
 
-                System.out.println("Ingrese el nombre del empleado: "); nombreempleado = sc.nextLine();
-                System.out.println("Ingrese el numero de documento: "); numerodocumento = sc.nextLine();
+                                break;
+                            case 2:
+                                System.out.print("Nombre: "); nombre = sc.nextLine();
+                                System.out.print("Puesto: "); puesto = sc.nextLine();
+                                do {
+                                    System.out.print("Salario: ");
+                                    if (sc.hasNextDouble()){
+                                        salario = sc.nextDouble();
+                                        esNumero = true;
+                                    }
+                                    else {
+                                        System.out.println("Ingrese un numero");
+                                        esNumero = false;
+                                        sc.next();
+                                    }
+                                } while (!(esNumero));
 
-                for (Empleado aux:emp.getPlanilla()) {
-                    if(nombreempleado.equals(aux.getNombre())){
-                        aux2=aux.getNombre();
-                    }
-                }
 
-                for (Documento aux: p.getDocumentos()) {
-                    if(numerodocumento.equals(aux.getNumero())){
-                        aux3=aux.getNumero();
-                    }
-                }
+                                do {
+                                    System.out.print("Meses de contrato: ");
+                                    if (sc.hasNextInt()){
+                                        mesescontrato = sc.nextInt();sc.nextLine();
+                                        esNumero = true;
+                                    }
+                                    else {
+                                        System.out.println("Ingrese un numero entero");
+                                        esNumero = false;
+                                        sc.next();
+                                    }
+                                } while (!(esNumero));
 
-                if (aux3==""){
-                    for (Documento aux: s.getDocumentos()) {
-                        if(numerodocumento.equals(aux.getNumero())){
-                            aux3=aux.getNumero();
+
+                                s=new ServicioProfesional(nombre, puesto, salario, mesescontrato);
+                                emp.addEmpleado(s);
+                                do{
+                                    System.out.print("Documento: "); documento = sc.nextLine();
+                                    System.out.print("Numero de documento: "); numerodocumento = sc.nextLine();
+                                    s.addDocumentos(new Documento(documento, numerodocumento));
+                                    System.out.println("¿Desea ingresar otro?(S/N)");
+                                    otro = sc.nextLine();
+                                }while (otro.equals("S"));
+
+                                break;
                         }
-                    }
+                        break;
 
-                    if(aux2=="" && aux3==""){
-                        System.out.println("Empleado o documento no encontrado");
-                    }else{
-                        emp.quitEmpleado(aux2);
-                        s.removeDocumento(aux3);
+                    case 2:
+                        String aux2="", aux3="";
 
-                    }
+                        System.out.println("Ingrese el nombre del empleado: "); nombreempleado = sc.nextLine();
+                        System.out.println("Ingrese el numero de documento: "); numerodocumento = sc.nextLine();
+
+                        for (Empleado aux:emp.getPlanilla()) {
+                            if(nombreempleado.equals(aux.getNombre())){
+                                aux2=aux.getNombre();
+                            }
+                        }
+
+                        for (Documento aux: p.getDocumentos()) {
+                            if(numerodocumento.equals(aux.getNumero())){
+                                aux3=aux.getNumero();
+                            }
+                        }
+
+                        if (aux3==""){
+                            for (Documento aux: s.getDocumentos()) {
+                                if(numerodocumento.equals(aux.getNumero())){
+                                    aux3=aux.getNumero();
+                                }
+                            }
+
+                            if(aux2=="" && aux3==""){
+                                System.out.println("Empleado o documento no encontrado");
+                            }else{
+                                emp.quitEmpleado(aux2);
+                                s.removeDocumento(aux3);
+
+                            }
+                        }
+
+                        else if(aux2=="" && aux3==""){
+                            System.out.println("Empleado o documento no encontrado");
+                        }else{
+                            emp.quitEmpleado(aux2);
+                            p.removeDocumento(aux3);
+
+                        }
+
+                        break;
+
+                    case 3:
+                        String cadena = "";
+
+                        for (Empleado aux:emp.getPlanilla()) {
+                            cadena+="\nEmpleado: "+aux.getNombre()+" Cargo: "+aux.getPuesto()+" Salario: "+aux.getSalario()+" ";
+                            if(aux instanceof PlazaFija){
+                                cadena+="Plaza Fija"+"\nDocumentos: " ;
+                            }
+                            else{
+                                cadena+="Servicio Profesional"+"\nDocumentos: ";
+                            }
+                            for (Documento aux1:aux.getDocumentos()) {
+                                cadena+="Nombre: "+aux1.getNombre()+" Numero: "+aux1.getNumero()+" ";
+                            }
+                        }
+                        System.out.println(cadena);
+                        break;
+
+                    case 4:
+                        System.out.println("Ingrese el nombre del empleado: ");
+                        String n = sc.nextLine();
+                        for (Empleado aux:emp.getPlanilla()) {
+                            if(aux.getNombre().equals(n)){
+                                System.out.println(CalculadoraImpuestos.calcularPago(aux));
+                            }
+                        }
+                        break;
+                    case 5:
+                        System.out.println(CalculadoraImpuestos.mostrarTotales());
+                        break;
+
+                    case 6:
+                        break;
+
+
+
                 }
 
-                else if(aux2=="" && aux3==""){
-                    System.out.println("Empleado o documento no encontrado");
-                }else{
-                    emp.quitEmpleado(aux2);
-                    p.removeDocumento(aux3);
-
-                }
-
-                break;
-
-            case 3:
-                String cadena = "";
-
-                for (Empleado aux:emp.getPlanilla()) {
-                    cadena+="\nEmpleado: "+aux.getNombre()+" Cargo: "+aux.getPuesto()+" Salario: "+aux.getSalario()+" ";
-                    if(aux instanceof PlazaFija){
-                        cadena+="Plaza Fija"+"\nDocumentos: " ;
-                    }
-                    else{
-                        cadena+="Servicio Profesional"+"\nDocumentos: ";
-                    }
-                    for (Documento aux1:aux.getDocumentos()) {
-                        cadena+="Nombre: "+aux1.getNombre()+" Numero: "+aux1.getNumero()+" ";
-                    }
-                }
-                System.out.println(cadena);
-                break;
-
-            case 4:
-                System.out.println("Ingrese el nombre del empleado: ");
-                String n = sc.nextLine();
-                for (Empleado aux:emp.getPlanilla()) {
-                    if(aux.getNombre().equals(n)){
-                        System.out.println(CalculadoraImpuestos.calcularPago(aux));
-                    }
-                }
-                break;
-            case 5:
-                System.out.println(CalculadoraImpuestos.mostrarTotales());
-                break;
-
-            case 6:
-                break;
-
-
-
-        }
-
+            }  else {
+                System.out.println("Ingrese un numero de las opciones");
+                opcion = 0;
+                sc.next();
+            }
 
         }
 
